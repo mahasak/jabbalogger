0a8c9cf731d7cde4703b24e07a916cb9ec39023f
Mahasak Pijittum <mahasak.pijittum@agoda.com>
2018-01-04 14:45:59 +0700

    change name to jabbalogger

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
     extensions: [ '.tsx', '.ts', '.js' ]
   },
   output: {
-    filename: 'scribe.js',
+    filename: 'jabbalogger.es6.js',
     path: path.resolve(__dirname, 'dist')
   }
 };
