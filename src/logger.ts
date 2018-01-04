3e89c1e03106f23822577b09d33aca6670b195bb
Mahasak Pijittum <mahasak.pijittum@agoda.com>
2018-01-04 22:18:26 +0700

    add logger

---
+++ src/logger.ts
@@ -0,0 +1,21 @@
+export class Logger {
+    fatal(message: Error|string, ...properties: any[]) {
+
+    }
+
+    error(message: Error|string, ...properties: any[]) {
+
+    }
+
+    warning(message: Error|string, ...properties: any[]) {
+
+    }
+    
+    info(message: Error|string, ...properties: any[]) {
+
+    }
+
+    debug(message: Error|string, ...properties: any[]) {
+
+    }
+}
