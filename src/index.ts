81d13a686648c6b3beb035023cc5cf8603085e06
Mahasak Pijittum <mahasak.pijittum@agoda.com>
2018-01-29 17:40:53 +0700

    export LocalStorageSink

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import './polyfills/objectAssign'
 import { ConsoleSink } from './sinks/console';
+import { LocalStorageSink} from './sinks/localStorage';
 import { Logger } from './logger';
 import { LoggerConfiguration } from './loggerFactory';
 
 export { Guid } from './guid';
 export * from './message'
 export { LogLevel } from './logLevel';
-export {Logger,LoggerConfiguration,ConsoleSink}
+export {Logger,LoggerConfiguration,ConsoleSink, LocalStorageSink}
